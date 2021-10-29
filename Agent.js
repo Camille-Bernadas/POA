98741144d54f7e08bbecc187e341ee4e95d2def2
Tristan-Tribes <43269571+Tristan-T@users.noreply.github.com>
2021-10-29 15:28:35 +0200

    updated agent

--- Agent.js
+++ Agent.js
@@ -8,17 +8,30 @@ class Agent {
     }
 
     move(x, y){
-      this.board.grid[this.x][this.y] = 1;
+      this.board.grid[this.x][this.y] = EMPTY;
       let newX = this.x+x;
       let newY = this.y+y;
+
+      //Out of bound
       if(newX < 0 || newX >= this.board.x || newY < 0 || newY >= this.board.y) {
         return;
       }
-      if(this.board.grid[newX][newY] == 1 || this.board.grid[newX][newY] == 3) {
-        this.x += x
-        this.y += y
+
+      //Movable
+      if(this.board.grid[newX][newY] == EMPTY || this.board.grid[newX][newY] == GOAL) {
+        this.x += x;
+        this.y += y;
       }
-      this.board.grid[this.x][this.y] = 2;
+
+      //Box handling
+      if(this.board.grid[newX][newY] == BOX) {
+        if(this.board.grid[newX+x][newY+y] == EMPTY) {
+          this.board.grid[newX][newY] = EMPTY;
+          this.board.grid[newX+x][newY+y] = BOX;
+        }
+      }
+
+      this.board.grid[this.x][this.y] = PLAYER;
       this.board.updateBoard(this.board.grid);
       console.log(this.x + "," + this.y);
       return;
