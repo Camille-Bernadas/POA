16219550fd3a00029e8c88a80a23473370109452
Camille BERNADAS <camillebernadas@gmail.com>
2021-11-19 16:47:42 +0100

    Save&Load Maps

---
+++ mapSaveLoader.js
@@ -0,0 +1,46 @@
+function saveMapToFile(){
+	let mapData = { "gameVersion" : version, "map" : [0], "height" : grid.length, "width" : grid[0].length};
+	for(let x =0; x < grid.length; x++){
+		mapData["map"][x] = [];
+		for(let y =0; y < grid[0].length; y++){
+			mapData["map"][x][y] = grid[x][y];
+		}
+	}
+	let mapAsString = JSON.stringify(mapData);
+
+	var map = new Blob([mapAsString], { type: 'application/json' });
+	var a = document.createElement('a');
+	a.download = 'map.json';
+	a.href = window.URL.createObjectURL(map);
+	a.click();
+}
+
+function loadMapToGame(){
+	let filecontent;
+	let file = document.getElementById("load").files[0];
+	if (file) {
+	    var reader = new FileReader();
+	    reader.readAsText(file, "UTF-8");
+	    reader.onload = function (evt) {
+	    	filecontent= JSON.parse(evt.target.result);
+
+	    	for(let x =0; x < filecontent.map.length; x++){
+				for(let y =0; y < filecontent.map[0].length; y++){
+					grid[x][y] = filecontent.map[x][y];
+				}
+			}
+			board.updateBoard(grid);
+	    }
+	}
+	
+	
+}
+
+
+document.getElementById("save").addEventListener('click', (event) => {
+  saveMapToFile();
+})
+
+document.getElementById("load").addEventListener('change', (event) => {
+  loadMapToGame();
+})
